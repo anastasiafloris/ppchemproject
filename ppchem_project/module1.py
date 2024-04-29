3b6d543ffe9436b0deeb1712553ac60626ed8265
Anastasia Floris <anastasiafloris@Anastasias-MacBook-Air.local>
2024-04-29 18:52:52 +0200

    Updated module1.py with RDKit calculations and drawings”

--- ppchem_project/module1.py
+++ ppchem_project/module1.py
@@ -13,6 +13,16 @@ age = -(half_life_c14) * math.log(remaining_ratio) / math.log(2)
 print (age)
 
 print(f"The age of the sample is approximately {age:.2f} years.")
+
+# project idea: Create a Python package that allows users to analyze chemical compounds, perform molecular computations, visualize molecular structures, and generate various plots related to chemical properties.
+# 1. Molecular Descriptors Calculation: RDKit to calculate molecular descriptors such as molecular weight, number of atoms, number of bonds, etc.
+# 2. Chemical Similarity Search: use of functions to perform chemical similarity searches based on molecular fingerprints or other similarity metrics.
+# 3. Molecule Drawing: Allows users to draw chemical structures using RDKit and visualize them using matplotlib.
+# 4. Data Import and Export: Providing functions to import chemical data from various file formats (e.g., SDF, SMILES) and export results to CSV or other formats.
+# 5. Visualization: Creation of functions to generate plots and visualizations of chemical properties, such as histograms of molecular weights, scatter plots of property distributions, or 3D molecular structures.
+
+
+
     
 
 
