61be9dc8d66831c8b6f4d2d294406691f1a8e076
anastasiafloris <160880304+anastasiafloris@users.noreply.github.com>
2024-05-07 17:55:19 +0200

    Update module1.py

--- ppchem_project/module1.py
+++ ppchem_project/module1.py
@@ -1,25 +1,9 @@
-#calculating the age of a sample using the Carbon-14 isotope knowing that the half life of carbon-14 is 5700 years
-    
-
- # Half-life of Carbon-14 in years
-import math
-half_life_c14 = 5700
-
-    # Assuming remaining ratio of Carbon-14 in the sample
-remaining_ratio = float(input("Enter the remaining ratio of Carbon-14 in the sample (between 0 and 1): "))
-
-# Calculate the age using the formula: age = -(half_life) * ln(remaining_ratio) / ln(2)
-age = -(half_life_c14) * math.log(remaining_ratio) / math.log(2)
-print (age)
-
-print(f"The age of the sample is approximately {age:.2f} years.")
-
-# project idea: Create a Python package that allows users to analyze chemical compounds, perform molecular computations, visualize molecular structures, and generate various plots related to chemical properties.
-# 1. Molecular Descriptors Calculation: RDKit to calculate molecular descriptors such as molecular weight, number of atoms, number of bonds, etc.
-# 2. Chemical Similarity Search: use of functions to perform chemical similarity searches based on molecular fingerprints or other similarity metrics.
-# 3. Molecule Drawing: Allows users to draw chemical structures using RDKit and visualize them using matplotlib.
-# 4. Data Import and Export: Providing functions to import chemical data from various file formats (e.g., SDF, SMILES) and export results to CSV or other formats.
-# 5. Visualization: Creation of functions to generate plots and visualizations of chemical properties, such as histograms of molecular weights, scatter plots of property distributions, or 3D molecular structures.
+# project idea: Create a Python package that allows users to analyze chemical compounds, perform molecular computations and visualize molecular structures.
+# 1. Molecular Descriptors Calculation: RDKit to calculate molecular descriptors such as molecular weight, number of atoms and number of bonds.
+# 2. Molecule Drawing: Allows users to draw chemical structures using RDKit.
+# 3. Data Import and Export: Providing functions to import chemical data as CSV files. 
+# 4. Visualization: 3D molecular interactive visulaization.
+# 5. Presentation: displaying the molecular characteristics using pandas.
 
 # use of RDKit to calculate molecular weight and molecular projection
 from rdkit import Chem
@@ -273,7 +257,7 @@ if __name__ == "__main__":
 
 
 
-# with dataset:
+# Final code, displaying 2D as well as interactive 3D projection. It displays molecular characteristics (Molecular Weight, Number of Atoms, Number of Bonds, Molecular Formula, IUPAC Name, Exact Mass, Monoisotopic Mass) based on a PubChem dataset:
 import pandas as pd
 from rdkit import Chem
 from rdkit.Chem import Draw
